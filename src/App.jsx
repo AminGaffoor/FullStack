97c786c1d22e3a1683c3299f1d469837238573ff
Amin <amingaffoor21@gmail.com>
2024-10-22 23:38:17 -0400

    Added form and table structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() { // Capitalize component names
+  // All components have to return some JSX — that's it
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1 className="testClass">Submit your fav links!</h1>
+
+      <form>
+        <label htmlFor="linkName">Link Name</label>
+        <input type="text" name="linkName" />
+        <br />
+        <label htmlFor="linkURL">Link URL</label>
+        <input type="text" name="linkURL" />
+      </form>
+
+      <table>
+      </table>
+    </div>
+  );
 }
+
 
 export default App
